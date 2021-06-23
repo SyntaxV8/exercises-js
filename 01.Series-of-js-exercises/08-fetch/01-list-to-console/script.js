c9153b186a43e5b35e710c59c779fb369284fa7e
SyntaxV8 <yordi.vt@hotmail.com>
2021-06-23 12:28:58 +0200

    Fetched the same data in 2 ways. Exercise Chapter 8 No.1

--- 01.Series-of-js-exercises/08-fetch/01-list-to-console/script.js
+++ 01.Series-of-js-exercises/08-fetch/01-list-to-console/script.js
@@ -7,6 +7,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
+(async () => {
+    let data = await fetch('http://localhost:63342/01.Series-of-js-exercises/_shared/api.json')
+    let main = await data.json();
+
+    console.log(main)
 })();
+
+thenFunction = () => {
+    fetch('http://localhost:63342/01.Series-of-js-exercises/_shared/api.json')
+        .then (response => response.json())
+        .then(data => console.log(data));
+}
+
+thenFunction();
