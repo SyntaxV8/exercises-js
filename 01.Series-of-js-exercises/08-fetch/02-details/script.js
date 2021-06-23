da350783f4b1707c8bdb8a4dc10a32c224de5d2b
SyntaxV8 <yordi.vt@hotmail.com>
2021-06-24 00:59:41 +0200

    Finalized exercise, learned to clone, learned to retrieve data and display it. Exercise Chapter 8 No.2

--- 01.Series-of-js-exercises/08-fetch/02-details/script.js
+++ 01.Series-of-js-exercises/08-fetch/02-details/script.js
@@ -9,4 +9,22 @@
 
 (() => {
     // your code here
+
+    document.getElementById('run').addEventListener('click', function Hero () {
+        let inputID = document.getElementById("hero-id").value;
+
+        fetch('http://localhost:63342/01.Series-of-js-exercises/_shared/api.json')
+            .then (response => { return response.json()})
+            .then(data => {
+                let hero = data.heroes.find(findHero=>findHero.id == inputID);
+                let templateClone = document.getElementById("tpl-hero").content.cloneNode(true);
+                document.getElementById('target').appendChild(templateClone);
+                document.querySelector('#target .name').innerHTML = hero.name;
+                document.querySelector('#target .alter-ego').innerHTML = hero.alterEgo;
+                document.querySelector('#target .powers').innerHTML = hero.abilities;
+
+            })
+    })
+
+
 })();
